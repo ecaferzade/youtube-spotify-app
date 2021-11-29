365890e1b4753354647ef18d3e4e979cacae0917
Emirali Caferzade <ecaferzade@hotmail.com>
2021-11-29 19:08:41 +0100

    code runs correctly. Docstrings are missing.

--- spotify_create_playlist.py
+++ spotify_create_playlist.py
@@ -3,26 +3,22 @@ import googleapiclient.discovery
 import os
 import pprint
 import requests
-from spotify_client_secret import client_id, client_secret
+from spotify_client_secret import client_id, client_secret, user_id
 import webbrowser
 import socket
+import json
 
 
-scopes = ["https://www.googleapis.com/auth/youtube.readonly"]
+scope_youtube = ["https://www.googleapis.com/auth/youtube.readonly"]
+scope_spotify = "playlist-modify-public playlist-modify-private user-read-email"
 
 
-# Function for cleaning the liked song names from Youtube
-def str_cleaner(str):
-    start = str.find( '(' )
-    end = str.find( ')' )
-    if (not start == -1) and (not end == -1):
-        str = str[0 : start :] + str[end+1 : -1]
-    str = str.replace("-","")
-    #str = str.replace(" ", "%20")
-    return str
-
-
-def get_youtube_client():
+def get_liked_vids_youtube(n):
+    """
+    Get permission of the user and collect the last n liked videos from YouTube.
+    :param n: Number of liked songs to collect
+    :return: list of last n liked videos
+    """
     # modified from YouTube Data API
     # Disable OAuthlib's HTTPS verification when running locally.
     # *DO NOT* leave this option enabled in production.
@@ -32,7 +28,7 @@ def get_youtube_client():
     client_secrets_file = "client_secrets.json"
     # Get credentials and create an API client
     flow = google_auth_oauthlib.flow.InstalledAppFlow.from_client_secrets_file(
-        client_secrets_file, scopes)
+        client_secrets_file, scope_youtube)
     try:
         credentials = flow.run_local_server(authorization_prompt_message="",
                                             redirect_uri_trailing_slash=False)
@@ -41,33 +37,36 @@ def get_youtube_client():
         request = youtube.videos().list(
             part="snippet",
             myRating="like",
-            maxResults=100
+            maxResults=n
         )
         response = request.execute()
-        list_of_liked_songs = []
+        list_of_liked_videos = []
         for item in response["items"]:
-            list_of_liked_songs.append(str_cleaner(item["snippet"]["title"]))
-        pprint.pprint(list_of_liked_songs)
+            list_of_liked_videos.append(item["snippet"]["title"])
     except OSError as err:
         print("\nPlease wait 60 seconds before running the script again. "
               "This is necessary due to the maximum packet age.")
         return 0
-    return response
+    return list_of_liked_videos
 
 
 def get_spotify_auth(cl_id):  # Chosen method: Authorization Code
+    """
+
+    :param cl_id:
+    :return:
+    """
     # Request User Authorization
     # Prepare the first GET request to take users permission
     host = "localhost"
     port = 9000
     auth_endpoint = "https://accounts.spotify.com/authorize"
     red_uri = "http://localhost:{}/".format(port)
-    scp = "playlist-modify-public playlist-modify-private"
     payload_get = {'client_id': cl_id,
                    'response_type': 'code',
                    'redirect_uri': red_uri,
                    'show_dialog': 'false',
-                   'scope': scp
+                   'scope': scope_spotify
                    }
     # After permission, a code will be sent.
     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
@@ -97,11 +96,70 @@ def get_spotify_auth(cl_id):  # Chosen method: Authorization Code
                             data=payload_post,
                             headers=headers_post
                             )
-    access_token = token_r.json()['access_token']
+    acs_token = token_r.json()['access_token']
+    refresh_token = token_r.json()['refresh_token']
+    print("Status code for token: ", token_r.status_code)
+    return acs_token
+
+
+def create_spotify_playlist(playlist_name, acs_token, usr_id):
+    create_playlist_endpoint = "https://api.spotify.com/v1/users/{}/playlists".format(usr_id)
+    payload_post = json.dumps({"name": "{}".format(playlist_name),
+                               "description": "This playlist is generated automatically by my youtube-spotify-app"
+                               })
+    headers_post = {"Accept": "application/json",
+                    "Content-Type": "application/json",
+                    "Authorization": "Bearer {}".format(acs_token)
+                    }
+    create_playlist_r = requests.post(create_playlist_endpoint,
+                                      data=payload_post,
+                                      headers=headers_post
+                                      )
+    print("Status code for playlist creation: ", create_playlist_r.status_code)
+    return create_playlist_r.json()['id']  # playlist id
+
+
+def search_on_spotify(liked_videos, acs_token):
+    # An undeniable part of the liked videos
+    # are not going to be found since they are basically not songs.
+    # Unfound videos should be returned.
+    spotify_song_uri = []
+    unfound_songs = []
+    search_endpoint = "https://api.spotify.com/v1/search"
+    for video in liked_videos:
+        headers_get = {"Authorization": "Bearer {}".format(acs_token)}
+        params_get = {"q": "{}".format(video),
+                      "type": "track",
+                      "limit": "1"}
+        song_search_r = requests.get(
+            search_endpoint,
+            params=params_get,
+            headers=headers_get)
+        if not json.loads(song_search_r.text)["tracks"]["items"] == []:  # track returned a result
+            spotify_song_uri.append(json.loads(song_search_r.text)["tracks"]["items"][0]["uri"])
+        else:
+            unfound_songs.append(video)
+    print("FOUND SONGS: ")
+    pprint.pprint(spotify_song_uri)
+    print("UNFOUND SONGS: ")
+    pprint.pprint(unfound_songs)
+    return spotify_song_uri
+
+
+def add_songs_to_playlist(acs_token, uris, plylist_id):
+    add_songs_endpoint = "https://api.spotify.com/v1/playlists/{}/tracks".format(plylist_id)
+    headers_post = {"Content-Type": "application/json",
+                            "Authorization": "Bearer {}".format(acs_token)}
+    payload_post = json.dumps({"uris": uris})
+    add_songs_r = requests.post(add_songs_endpoint,
+                                data=payload_post,
+                                headers=headers_post)
+    print("Status code for adding the songs: ", add_songs_r.status_code)
 
 
 if __name__ == "__main__":
-    #get_youtube_client()
-    get_spotify_auth(client_id)
-
-
+    liked_vids_on_youtube = get_liked_vids_youtube(10)  # Get permission and liked vids from YouTube
+    access_token = get_spotify_auth(client_id)
+    playlist_id = create_spotify_playlist("auto-gen-playlist", access_token, user_id)
+    found_songs = search_on_spotify(liked_vids_on_youtube, access_token)
+    add_songs_to_playlist(access_token, found_songs, playlist_id)
